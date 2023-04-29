b81ad9164eb54d291faedae29783d8902a700c10
Kaewmanee Marasri <64160234@go.buu.ac.th>
2023-04-29 19:29:04 +0700

    :tada: mock up background home page

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:gap/gap.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -13,10 +14,22 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Center(child: Text("Home")),
+      body: Container(
+        width: double.infinity,
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [
+            Color.fromARGB(255, 232, 160, 191),
+            Color.fromARGB(200, 186, 144, 198),
+          ],
+        )),
+        padding: EdgeInsets.only(top: 70.0, left: 15.0),
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [Text("Hello"), Gap(15), Text("Kaewmanee")]),
       ),
-      body: const Center(child: Text("This is the home page")),
     );
   }
 }

eade0bc00541df2374105f15acda54557c45aa25
Kaewmanee Marasri <64160234@go.buu.ac.th>
2023-04-29 17:23:16 +0700

    :memo: create  project

---
+++ android/app/src/main/kotlin/com/example/lucky_with_you/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.lucky_with_you
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screen/loading_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter/widgets.dart';
+import 'package:gap/gap.dart';
+
+class LoadingScreen extends StatelessWidget {
+  const LoadingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          Text("Image"),
+          Gap(15),
+          Text("Loading..."),
+        ]),
+      ),
+    );
+  }
+}

da70d36b70c41ea8653b2e326b1200a82080248a
Kaewmanee Marasri <64160234@go.buu.ac.th>
2023-05-01 17:33:15 +0700

    ✏️ create class about card

---
+++ lib/data/Card.dart
@@ -0,0 +1,8 @@
+import 'package:lucky_with_you/data/description.dart';
+
+class Card {
+  const Card({required this.name, required this.image, required this.des});
+  final String name;
+  final String image;
+  final Description des;
+}
---
+++ lib/data/description.dart
@@ -0,0 +1,11 @@
+class Description {
+  const Description(
+      {required this.descriptionOfLove,
+      required this.descriptionOfHealth,
+      required this.descriptionOfStudy,
+      required this.descriptionOfToDay});
+  final String descriptionOfLove;
+  final String descriptionOfHealth;
+  final String descriptionOfStudy;
+  final String descriptionOfToDay;
+}
